8d4299b21d7ac30403cd26b55cb2e72b9618c7ad
KipHub <remo.auvaart@gmail.com>
2024-07-02 15:06:39 +0300

    Improve AutosavePopupGui visuals

--- source/interface/autosave_popup_gui.cpp
+++ source/interface/autosave_popup_gui.cpp
@@ -2,26 +2,27 @@
 
 void AutosavePopupGui::draw(Window& window, Input& input)
 {
-	if (ImGui::BeginPopupModal("Autosave detected", NULL, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove))
+	if (ImGui::BeginPopupModal("Autosave detected", NULL, ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove))
 	{
+        const ImVec2 buttonSize = ImVec2(160.0f, 0.0f);
+        if (ImGui::Button("Open autosave", buttonSize))
+        {
+            openAutosaveEvent.invoke();
+            ImGui::CloseCurrentPopup();
+        }
         if (ImGui::BeginItemTooltip())
         {
             ImGui::Text("Note: You should only use the autosave this way temporarily, then open the actual file again.");
             ImGui::EndTooltip();
         }
-        if (ImGui::Button("Open autosave"))
-        {
-            openAutosaveEvent.invoke();
-            ImGui::CloseCurrentPopup();
-        }
         ImGui::SameLine();
-        if (ImGui::Button("Copy to file and open"))
+        if (ImGui::Button("Copy to and open file", buttonSize))
         {
             applyAutosaveOpenFileEvent.invoke();
             ImGui::CloseCurrentPopup();
         }
         ImGui::SameLine();
-        if (ImGui::Button("Ignore autosave, open file"))
+        if (ImGui::Button("Ignore and open file", buttonSize))
         {
             ignoreAutosaveOpenFileEvent.invoke();
             ImGui::CloseCurrentPopup();
